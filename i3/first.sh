e5593fb47cddf5bee978b330ddfa5811a430e18d
Carlos Coelho <carlos@vinta.com.br>
2018-01-10 18:07:33 -0300

    Add i3 config
    Signed-off-by: Carlos Coelho <carlos@vinta.com.br>

---
+++ i3/first.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+i3status --config ~/.config/i3/first.conf | while :
+do
+    read line
+
+    dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+    spotify_status=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'PlaybackStatus' | tail -n1 | cut -d'"' -f2)
+    spotify_artist=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/spotify_song.awk | head -n 1 | cut -d':' -f2)
+    spotify_song=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | awk -f ${dir}/spotify_song.awk | tail -n 1 | cut -d':' -f2)
+    mpd_song=$(mpc current)
+
+    if [ "$spotify_status" = "Playing" ] ; then
+        echo -n " $spotify_artist - $spotify_song | $line" || exit 1
+        continue
+    elif [ "$mpd_song" != "" ]; then
+        mpd_status=$(mpc status | tail -2 | head -1 | cut -d' ' -f1 | tr -d '[]')
+        if [ "$mpd_status" = "playing" ]; then            
+            echo -n "NP: $mpd_song | $line" || exit 1
+            continue
+        fi
+        echo -n $line || exit 1
+        continue
+    fi
+    echo -n $line || exit 1
+done
+~
