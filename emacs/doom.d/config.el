371f9cb3e541b2df66c505250b25acfd1dc72ace
Carlos Coelho <chocoelho@outlook.com.br>
2022-02-15 01:13:47 -0300

    Update banner on emacs config
    Signed-off-by: Carlos Coelho <chocoelho@outlook.com.br>

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -55,3 +55,34 @@
 
 ;; Define where projectile should look for projects
 (setq projectile-project-search-path '("~/src"))
+
+(defun doom-dashboard-draw-custom-ascii-banner-fn ()
+  (let* ((banner
+          '("
+
+
+████          ████   ███        ████        ███   █████████████     █████
+ ████        ████    ███        ██████      ███   █████████████    ███████
+                                   ████     ███        ███        ████ ███
+  ████      ████     ███        ██  ████    ███        ███       ████   ███
+   ████    ████      ███        ███   ███   ███        ███      ████    ████
+    ███   ████       ███        ███    ████  ██        ███     ████      ████
+     ███ ████        ███        ███     ████
+     ███████    █████████████   ███      ██████        ███   ████          ████
+      █████     █████████████   ███        ████        ███  ████            ████
+
+"))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat
+                 line (make-string (max 0 (- longest-line (length line)))
+                                   32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+
+(setq +doom-dashboard-functions (cdr +doom-dashboard-functions))
+(push 'doom-dashboard-draw-custom-ascii-banner-fn +doom-dashboard-functions)
