c6665cc06c7d0229456efad9620652ce20085289
Carlos Coelho <chocoelho@outlook.com.br>
2022-04-08 11:16:49 -0300

    fix(emacs): use doom default banner
    Signed-off-by: Carlos Coelho <chocoelho@outlook.com.br>

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -51,27 +51,9 @@
 ;; they are implemented.
 
 ;; Define where projectile should look for projects
-(setq projectile-project-search-path '(("~/src" . 2)))
+(setq projectile-project-search-path '(("<PROJECTS_PATH>" . 2)))
 
-(defun doom-dashboard-draw-custom-ascii-banner-fn ()
-  (let* ((banner
-          '(""))
-         (longest-line (apply #'max (mapcar #'length banner))))
-    (put-text-property
-     (point)
-     (dolist (line banner (point))
-       (insert (+doom-dashboard--center
-                +doom-dashboard--width
-                (concat
-                 line (make-string (max 0 (- longest-line (length line)))
-                                   32)))
-               "\n"))
-     'face 'doom-dashboard-banner)))
-
-(setq +doom-dashboard-functions (cdr +doom-dashboard-functions))
-(push 'doom-dashboard-draw-custom-ascii-banner-fn +doom-dashboard-functions)
-
-(global-wakatime-mode)
+;; prospector linting
 (flycheck-prospector-setup)
 (setq +lsp-company-backend '(company-lsp :with company-tabnine :separate))
 (after! company
